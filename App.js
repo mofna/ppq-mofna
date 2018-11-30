ee4d69fac2ff6a8f42bb89579ed50d250090f751
Mofu <mofu.fna@gmail.com>
2018-12-01 01:16:28 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+﻿//ログイン処理
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+client.on('ready', () => {
+    console.log('ready...');
+});
+//Bot自身の発言を無視する呪い
+client.on('message', message =>{
+    if(message.author.bot){ return; }
+//↓ここに後述のコードをコピペする↓
+	if (message.content === '寒いね') {
+    		let channel = message.channel;
+	let author = message.author.username;
+	let reply_text = `寒いね`;
+	message.reply(reply_text)
+	.then(message => console.log(`Sent message: ${reply_text}`))
+		.catch(console.error);
+	return;
+   }
+//↑ここに後述のコードをコピペする↑
+});
+client.login(token);
